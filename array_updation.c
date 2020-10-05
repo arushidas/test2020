c09a4acaae27b01f480d1d8bc71f590147918769
Arushi Das <30744880+arushidas@users.noreply.github.com>
2020-10-05 14:35:49 +0530

    Create array_updation.c

---
+++ array_updation.c
@@ -0,0 +1,9 @@
+for(i = 0; i<n; i++) {
+printf("LA[%d] = %d \n", i, LA[i]);
+}
+LA[k-1] = item;
+printf("The array elements after updation :\n");
+for(i = 0; i<n; i++) {
+printf("LA[%d] = %d \n", i, LA[i]);
+}
+}
